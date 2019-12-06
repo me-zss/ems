df260b67488057d422527ba5475fb53284a6556e
shun <1610252531@vip.henu.edu.cn>
2019-12-06 14:52:15 +0800

    添加实体类-Dept

---
+++ src/main/java/com/shun/entity/Dept.java
@@ -0,0 +1,17 @@
+package com.shun.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Dept implements Serializable {
+    private String id;
+    private String name;
+}
