0ffca72a540f9cb85609d0789143803fe03a5fb2
shun <1610252531@vip.henu.edu.cn>
2019-12-06 14:50:51 +0800

    添加实体类-User

---
+++ src/main/java/com/shun/entity/User.java
@@ -0,0 +1,21 @@
+package com.shun.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class User implements Serializable {
+    private String id;
+    private String username;
+    private String name;
+    private String password;
+    private String sex;
+    private String salt;
+}
