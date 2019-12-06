82c44bb6016649221546fd0b35874359d6d42b52
shun <1610252531@vip.henu.edu.cn>
2019-12-06 14:53:22 +0800

    添加类注解

--- src/main/java/com/shun/entity/Dept.java
+++ src/main/java/com/shun/entity/Dept.java
@@ -7,6 +7,9 @@ import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
+/**
+ * @author shun
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/shun/entity/Emp.java
+++ src/main/java/com/shun/entity/Emp.java
@@ -9,6 +9,9 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+/**
+ * @author shun
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
