0f3d877fba3e2526c51f2ff07c7c23ba7d0acd5d
shun <1610252531@vip.henu.edu.cn>
2019-12-06 15:55:10 +0800

    添加MapperScanzhu注解

--- src/main/java/com/shun/EmsApplication.java
+++ src/main/java/com/shun/EmsApplication.java
@@ -2,11 +2,13 @@ package com.shun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author shun
  */
 @SpringBootApplication
+@MapperScan("com.shun.dao")
 public class EmsApplication {
 
     public static void main(String[] args) {
