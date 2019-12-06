eb91ae48e673345c082b7e305f13106df91415a3
shun <1610252531@vip.henu.edu.cn>
2019-12-06 14:42:53 +0800

    初始化配置

---
+++ src/main/java/com/shun/EmsApplication.java
@@ -0,0 +1,16 @@
+package com.shun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shun
+ */
+@SpringBootApplication
+public class EmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shun/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shun.ems;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
