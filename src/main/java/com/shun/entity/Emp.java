7c3ad2ff1d80e47319b17be0bc4802a4dfb173c1
shun <1610252531@vip.henu.edu.cn>
2019-12-06 14:52:38 +0800

    添加实体类-Emp

---
+++ src/main/java/com/shun/entity/Emp.java
@@ -0,0 +1,23 @@
+package com.shun.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Emp implements Serializable {
+    private String id;
+    private String name;
+    private BigDecimal salary;
+    private int age;
+    private Date bir;
+    private Dept dept;
+}
